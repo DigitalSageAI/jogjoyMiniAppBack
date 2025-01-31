7503d99c1ef6b7151cf1424f425971d31d4f84f5
AbylayNurtayev <krutyev6@gmail.com>
2025-01-31 12:32:00 +0500

    Удалил node_modules из репозитория

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -7,12 +7,17 @@ import { S3Client, PutObjectCommand, GetObjectCommand, DeleteObjectCommand } fro
 import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
 import mongoose from 'mongoose';
 import OpenAI from 'openai';
+import dotenv from 'dotenv'
+dotenv.config();
 
 
 const bucketName = process.env.BUCKET_NAME;
 const bucketRegion = process.env.BUCKET_REGION;
 const accessKey = process.env.ACCESS_KEY;
 const secretAccessKey = process.env.SECRET_ACCESS_KEY;
+
+
+
 
 const s3 = new S3Client({
   credentials: {
